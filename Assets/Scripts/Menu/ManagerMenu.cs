348ad9ec8b4507e23ba287e46b994ff52190fcbf
Katherin_Maria <Katitochara@gmail.com>
2023-10-20 14:41:17 -0500

    1ra Modificación

--- Assets/Scripts/Menu/ManagerMenu.cs
+++ Assets/Scripts/Menu/ManagerMenu.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,7 +6,48 @@ using UnityEngine.SceneManagement;
 public class ManagerMenu : MonoBehaviour
 {
     [SerializeField] GameObject PanelNotificacionSalir;
-    [SerializeField] string Nivel1;  
+    [SerializeField] string Nivel1;
+
+    [SerializeField] string EcenarioNivel1;
+    [SerializeField] string EcenarioNivel2;
+    [SerializeField] string EcenarioNivel3;
+    [SerializeField] string EcenarioNivel4;
+    [SerializeField] string EcenarioNivel5;
+
+
+    public void SeleccionNivel1()
+    {
+        SceneManager.LoadScene(EcenarioNivel1);
+        Debug.Log("Abriendo Nivel 1...");
+        Time.timeScale = 1f;
+    }
+    public void SeleccionNivel2()
+    {
+        SceneManager.LoadScene(EcenarioNivel2);
+        Debug.Log("Abriendo Nivel 2...");
+        Time.timeScale = 1f;
+    }
+    public void SeleccionNivel3()
+    {
+        SceneManager.LoadScene(EcenarioNivel3);
+        Debug.Log("Abriendo Nivel 3...");
+        Time.timeScale = 1f;
+    }
+    public void SeleccionNivel4()
+    {
+        SceneManager.LoadScene(EcenarioNivel4);
+        Debug.Log("Abriendo Nivel 4...");
+        Time.timeScale = 1f;
+    }
+    public void SeleccionNivel5()
+    {
+        SceneManager.LoadScene(EcenarioNivel5);
+        Debug.Log("Abriendo Nivel 5...");
+        Time.timeScale = 1f;
+    }
+
+
+
 
     public void ActivarNotificacionSalir()
     {
